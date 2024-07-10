223618199e5fadc1fba3b5d8b3796c9537375cc3
Manan Shah <shahmanan1014@gmail.com>
2024-07-10 20:15:37 +0530

    ok

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-let x = mongoose.connect("mongodb+srv://Manan:Manan@cluster0.vv4lzi6.mongodb.net/expanse");
+let x = mongoose.connect("mongodb://Manan:Manan@ac-a5o3slu-shard-00-00.vv4lzi6.mongodb.net:27017,ac-a5o3slu-shard-00-01.vv4lzi6.mongodb.net:27017,ac-a5o3slu-shard-00-02.vv4lzi6.mongodb.net:27017/?replicaSet=atlas-j311v7-shard-0&ssl=true&authSource=admin");
 // Connect to MongoDB database
 x.then(() => {
     console.log("connection ready successfully");
